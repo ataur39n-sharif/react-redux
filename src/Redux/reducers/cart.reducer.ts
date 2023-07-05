4e76914fbb3acbd6232f33848d56493c800de4d8
Ataur Rahman <rahman.sharif39n@gmail.com>
2023-07-05 13:17:56 +0600

    initial setup for cart reducer

---
+++ src/Redux/reducers/cart.reducer.ts
@@ -0,0 +1,36 @@
+import {AnyAction} from "redux";
+import {CartActionTypes} from "../actionTypes/cart.actionTypes.ts";
+
+export type TCartProduct={
+    image:string,
+    title:string,
+    subtitle:string,
+    price:number,
+    quantity:number
+}
+export type TInitialState={
+    products:TCartProduct[],
+    shippingCost:number,
+    discount: number,
+    subTotal: number,
+    total: number,
+}
+const initialState:TInitialState={
+    products:[],
+    subTotal:0,
+    shippingCost:0,
+    discount:0,
+    total:0,
+}
+
+export const CartReducers =(state=initialState,action:AnyAction)=>{
+    switch(action.type){
+        case CartActionTypes.ADD_TO_CART:
+            return {
+                ...state,
+                products:[...state.products,action.payload]
+            }
+        default:
+           return  state
+    }
+}
