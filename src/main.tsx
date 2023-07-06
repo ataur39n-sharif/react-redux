a28e167a5f8f44366782f746bbf1c1c6b2d0285b
Ataur Rahman <rahman.sharif39n@gmail.com>
2023-07-06 13:04:36 +0600

    next analytics added

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import App from './App.tsx'
 import './index.css'
 import {Provider} from "react-redux";
 import {store} from "./Redux/store.ts";
+import {Analytics} from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+        <Analytics/>
+    </React.StrictMode>,
 )
